cf8b077b88c4cf5d7b1ae53c81cfb64574c93db7
jwalter <jwalter@student.ethz.ch>
2018-03-07 09:34:16 +0100

    simple megumin armor 2

--- src/main/java/moe/karpador/patriot/ItemMeguminCloths.java
+++ src/main/java/moe/karpador/patriot/ItemMeguminCloths.java
@@ -1,4 +1,23 @@
 package moe.karpador.patriot;
 
-public class ItemMeguminCloths {
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+
+public class ItemMeguminCloths extends ItemArmor {
+    public String name;
+
+    public ItemMeguminCloths(ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn, String itemName) {
+        super(materialIn, renderIndexIn, equipmentSlotIn);
+        name = itemName;
+        setCreativeTab(Patriot.PATRIOT_TAB);
+        //this.setUnlocalizedName(ItemMeguminCloths.name);
+        setRegistryName(new ResourceLocation(Patriot.MODID, name));
+    }
+
+    @Override
+    public String getUnlocalizedName(ItemStack stack) {
+        return "item." + Patriot.RESOURCE_PREFIX + name; //item.patriot:armor_megumin_hat
+    }
 }
--- src/main/java/moe/karpador/patriot/ModItems.java
+++ src/main/java/moe/karpador/patriot/ModItems.java
@@ -2,7 +2,9 @@ package moe.karpador.patriot;
 
 import net.minecraft.client.renderer.ItemModelMesher;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
@@ -16,6 +18,12 @@ public class ModItems {
     public static ItemPatriot itemPatriot;
     public static ItemMeguminStaff itemMeguminStaff;
 
+    public static ItemArmor.ArmorMaterial meguminMaterial = ItemArmor.ArmorMaterial.LEATHER; // EnumHelper.addArmorMaterial()
+    public static ItemMeguminCloths itemMeguminHat;
+    public static ItemMeguminCloths itemMeguminChest;
+    public static ItemMeguminCloths itemMeguminSkirt;
+    public static ItemMeguminCloths itemMeguminShoes;
+
     public static List<Item> items = new ArrayList<>();
 
     public static void init() {
@@ -26,6 +34,15 @@ public class ModItems {
         itemMeguminStaff = new ItemMeguminStaff();
         itemMeguminStaff.setRegistryName(new ResourceLocation(Patriot.MODID, ItemMeguminStaff.NAME));
         items.add(itemMeguminStaff);
+
+        itemMeguminHat = new ItemMeguminCloths(meguminMaterial, 1, EntityEquipmentSlot.HEAD, "armor_megumin_hat");
+        itemMeguminChest = new ItemMeguminCloths(meguminMaterial, 1, EntityEquipmentSlot.CHEST, "armor_megumin_chest");
+        itemMeguminSkirt = new ItemMeguminCloths(meguminMaterial, 2, EntityEquipmentSlot.LEGS, "armor_megumin_skirt");
+        itemMeguminShoes = new ItemMeguminCloths(meguminMaterial, 1, EntityEquipmentSlot.FEET, "armor_megumin_shoes");
+        items.add(itemMeguminHat);
+        items.add(itemMeguminChest);
+        items.add(itemMeguminSkirt);
+        items.add(itemMeguminShoes);
     }
 
     @SideOnly(Side.CLIENT)
@@ -37,6 +54,19 @@ public class ModItems {
         ModelResourceLocation modelItemMeguminStaff = new ModelResourceLocation(String.format("%s%s", Patriot.RESOURCE_PREFIX, ItemMeguminStaff.NAME), "inventory");
         ModelLoader.registerItemVariants(itemMeguminStaff, modelItemMeguminStaff);
         mesher.register(itemMeguminStaff, 0, modelItemMeguminStaff);
+
+        registerArmor(itemMeguminHat,mesher);
+        registerArmor(itemMeguminChest,mesher);
+        registerArmor(itemMeguminSkirt,mesher);
+        registerArmor(itemMeguminShoes,mesher);
+    }
+
+    private static void registerArmor(ItemMeguminCloths item, ItemModelMesher mesher) {
+        //ModelResourceLocation modelItem = new ModelResourceLocation(String.format("%s%s", Patriot.RESOURCE_PREFIX, item.name), "inventory");
+        //ModelLoader.registerItemVariants(item, modelItem);
+        //ModelResourceLocation modelItem = new ModelResourceLocation(String.format("%s%s", Patriot.RESOURCE_PREFIX, item.name), "inventory");
+        ModelLoader.setCustomModelResourceLocation(item,0,new ModelResourceLocation(Patriot.MODID,item.name));
+        //mesher.register(item, 0, modelItem);
     }
 
 
