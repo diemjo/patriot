2c0b3d863e8566eb0d46f60e5e5a47955ef5e91d
diemjo <j.diem@me.com>
2018-05-21 00:29:18 +0200

    get pantsu when right clicking a player, (not the final functionality yet)

--- src/main/java/moe/karpador/patriot/items/ItemStealMagic.java
+++ src/main/java/moe/karpador/patriot/items/ItemStealMagic.java
@@ -1,9 +1,16 @@
 package moe.karpador.patriot.items;
 
 import moe.karpador.patriot.Patriot;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
 
 public class ItemStealMagic extends Item {
     public static final String NAME = "item_steal_magic";
@@ -21,5 +28,37 @@ public class ItemStealMagic extends Item {
     @Override
     public String getUnlocalizedName() {
         return String.format("item.%s%s", Patriot.RESOURCE_PREFIX, NAME); //item.patriot:item_steal_magic
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+        if (world.isRemote) {
+            RayTraceResult res = Minecraft.getMinecraft().objectMouseOver;
+            if (res != null && res.typeOfHit == RayTraceResult.Type.ENTITY) {
+                //player.sendMessage(new TextComponentString("Entity: "+res.entityHit));
+                if (res.entityHit instanceof EntityPlayer) {
+                    if (getNumberOfMeguminClothes((EntityPlayer) res.entityHit) == 4) {
+                        player.inventory.addItemStackToInventory(new ItemStack(ModItems.itemMeguminPantsu));
+                    }
+                    else {
+                        player.inventory.addItemStackToInventory(new ItemStack(ModItems.itemGenericPantsu));
+                    }
+                }
+                else {
+                    player.sendMessage(new TextComponentString("you don't want to eat "+res.entityHit.getName()+" pantsu, do you?"));
+                }
+            }
+        }
+        return super.onItemRightClick(world, player, hand);
+    }
+
+    public int getNumberOfMeguminClothes(EntityPlayer player) {
+        int count = 0;
+        for (ItemStack item : player.getArmorInventoryList()) {
+            if (item.getItem() instanceof ItemMeguminCloths) {
+                count++;
+            }
+        }
+        return count;
     }
 }
