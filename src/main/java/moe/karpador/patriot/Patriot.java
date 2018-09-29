32a36ad1940737c779dae58e1e537b966dff1df5
jwalter <jwalter@student.ethz.ch>
2018-09-29 22:48:39 +0200

    When player joins the world, send him the "hasPantsu" field from the mana capability from all players currently in the world

--- src/main/java/moe/karpador/patriot/Patriot.java
+++ src/main/java/moe/karpador/patriot/Patriot.java
@@ -3,13 +3,19 @@ package moe.karpador.patriot;
 import moe.karpador.patriot.items.ModItems;
 import moe.karpador.patriot.mana.IMana;
 import moe.karpador.patriot.mana.ManaProvider;
+import moe.karpador.patriot.network.PantsuMessage;
+import moe.karpador.patriot.network.PatriotPacketHandler;
 import moe.karpador.patriot.proxy.CommonProxy;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -18,6 +24,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Patriot.MODID, name = Patriot.NAME, version = Patriot.VERSION)
@@ -70,8 +77,23 @@ public class Patriot {
                 KeyBinding.unPressAllKeys();
             }
         }
+    }
+    @SubscribeEvent
+    public static void onEntityJoinWorld(EntityJoinWorldEvent event) {
+        if(!event.getWorld().isRemote && event.getEntity() instanceof EntityPlayer) {
+            EntityPlayer newPlayer = (EntityPlayer) event.getEntity();
+            // send the hasPantsu field from the mana capability of all currently playing players to the new player
+            for (EntityPlayer player : event.getWorld().playerEntities) {
+                IMana mana = player.getCapability(ManaProvider.MANA_CAP, null);
+                if(mana != null) {
+                    PatriotPacketHandler.wrapper.sendTo(new PantsuMessage(true, !mana.hasPantsu(), player), (EntityPlayerMP) newPlayer); // on server you can just cast to EntityPlayerMP
+                }
+
+            }
+        }
 
     }
+
 
     public static final CreativeTabs PATRIOT_TAB = new CreativeTabs(Patriot.MODID) {
         @Override
