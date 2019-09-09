fa02faeae799e52c56a8d7e31e9272fb4123f4c1
jwalter <jwalter@student.ethz.ch>
2019-09-09 19:11:01 +0200

    change pantsu model and add textures for generic and megumin pantsu

--- src/main/java/moe/karpador/patriot/items/PantsuModel.java
+++ src/main/java/moe/karpador/patriot/items/PantsuModel.java
@@ -15,25 +15,33 @@ public class PantsuModel extends ModelBiped {
     public ModelRenderer shape17_1;
     public ModelRenderer shape17_2;
     public ModelRenderer shape17_3;
+    public ModelRenderer shape21;
+    public ModelRenderer shape21_1;
 
     public PantsuModel() {
         this.textureWidth = 64;
         this.textureHeight = 64;
-        this.shape17 = new ModelRenderer(this, 22, 9);
-        this.shape17.setRotationPoint(0.0F, -6.0F, -5.0F);
-        this.shape17.addBox(-5.0F, 0.0F, 0.0F, 10, 2, 10, 0.0F);
-        this.shape16 = new ModelRenderer(this, 0, 20);
+        this.shape16 = new ModelRenderer(this, 0, 0);
         this.shape16.setRotationPoint(0.0F, -9.0F, 0.0F);
         this.shape16.addBox(-1.0F, 0.0F, -4.5F, 2, 1, 9, 0.0F);
-        this.shape17_1 = new ModelRenderer(this, 13, 21);
+        this.shape17_1 = new ModelRenderer(this, 0, 12);
         this.shape17_1.setRotationPoint(0.0F, -7.0F, -5.0F);
-        this.shape17_1.addBox(-3.5F, 0.0F, 0.0F, 7, 1, 10, 0.0F);
-        this.shape17_3 = new ModelRenderer(this, 0, 19);
+        this.shape17_1.addBox(-3.0F, 0.0F, 0.0F, 6, 1, 10, 0.0F);
+        this.shape17_3 = new ModelRenderer(this, 42, 0);
         this.shape17_3.setRotationPoint(0.0F, -8.0F, 4.0F);
-        this.shape17_3.addBox(-2.5F, 0.0F, 0.0F, 5, 1, 1, 0.0F);
-        this.shape17_2 = new ModelRenderer(this, 0, 19);
+        this.shape17_3.addBox(-2.0F, 0.0F, 0.0F, 4, 1, 1, 0.0F);
+        this.shape21 = new ModelRenderer(this, 42, 2);
+        this.shape21.setRotationPoint(0.0F, -9.0F, -4.5F);
+        this.shape21.addBox(-1.5F, 0.0F, 0.0F, 3, 1, 2, 0.0F);
+        this.shape21_1 = new ModelRenderer(this, 52, 2);
+        this.shape21_1.setRotationPoint(0.0F, -9.0F, 2.5F);
+        this.shape21_1.addBox(-1.5F, 0.0F, 0.0F, 3, 1, 2, 0.0F);
+        this.shape17 = new ModelRenderer(this, 12, 0);
+        this.shape17.setRotationPoint(0.0F, -6.0F, -5.0F);
+        this.shape17.addBox(-5.0F, 0.0F, 0.0F, 10, 2, 10, 0.0F);
+        this.shape17_2 = new ModelRenderer(this, 52, 0);
         this.shape17_2.setRotationPoint(0.0F, -8.0F, -5.0F);
-        this.shape17_2.addBox(-2.5F, 0.0F, 0.0F, 5, 1, 1, 0.0F);
+        this.shape17_2.addBox(-2.0F, 0.0F, 0.0F, 4, 1, 1, 0.0F);
 
         bipedHead.cubeList.clear();
         bipedHeadwear.cubeList.clear();
@@ -43,6 +51,8 @@ public class PantsuModel extends ModelBiped {
         bipedHead.addChild(shape17_1);
         bipedHead.addChild(shape17_2);
         bipedHead.addChild(shape17_3);
+        bipedHead.addChild(shape21);
+        bipedHead.addChild(shape21_1);
     }
 
     @Override
@@ -50,12 +60,14 @@ public class PantsuModel extends ModelBiped {
         super.render(entity, f, f1, f2, f3, f4, f5);
         setRotationAngles(f, f1, f2, f3, f4, f5, entity);
         /*
-        this.shape17.render(f5);
         this.shape16.render(f5);
         this.shape17_1.render(f5);
         this.shape17_3.render(f5);
+        this.shape21.render(f5);
+        this.shape21_1.render(f5);
+        this.shape17.render(f5);
         this.shape17_2.render(f5);
-         */
+        */
     }
 
     /**
