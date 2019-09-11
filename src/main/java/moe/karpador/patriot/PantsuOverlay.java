4a40a80af2664fb4dc61abfb109da68fae545fc3
diemjo <j.diem@me.com>
2019-09-12 00:26:00 +0200

    added @SideOnly annotation to pantsu overlay

--- src/main/java/moe/karpador/patriot/PantsuOverlay.java
+++ src/main/java/moe/karpador/patriot/PantsuOverlay.java
@@ -12,7 +12,10 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
 public class PantsuOverlay extends Gui {
     private Minecraft mc;
 
